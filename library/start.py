6ab8c6e16ccf4cb83713940bb80d80c030d9bf1d
hernandezb3 <brittney.hernandez@uconn.edu>
2025-02-24 09:34:46 -0500

    update working hpc script

--- library/start.py
+++ library/start.py
@@ -1,9 +1,7 @@
+import os
 
-#MAIN_DIR = ""
-MAIN_DIR = "/Users/brittneyhernandez/Library/CloudStorage/OneDrive-SharedLibraries-YaleUniversity/Zieher, Almut - OAMM_PEACE23-24/"
-
-RAW_DIR = MAIN_DIR + "DIdentifiedData/Scores&Recalls/"
+MAIN_DIR = os.getcwd() + "/"
 
 DATA_DIR = MAIN_DIR + "data/"
 
-RESULTS_DIR = MAIN_DIR + "results/"
+OUTPUT_DIR = MAIN_DIR + "out/"
--- pilot_classifications_llama_hf_hpc.py
+++ pilot_classifications_llama_hf_hpc.py
@@ -8,27 +8,29 @@ import datetime # couldn't install use datetime
 from datetime import datetime
 import psutil
 import pandas as pd
-import ollama
-from ollama import chat
-from ollama import ChatResponse
-from library import start, secrets, classify, fit # take out oamm. to run whole file
+from huggingface_hub import login
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
+import torch
+from library import start, secrets, fit # take out oamm. to run whole file
 
-checkpoint0 = datetime.now() # start runtime counte
+checkpoint0 = datetime.now() # start runtime counter
 
 # llama models:
-# model 3.3 70B --
-#model_id = "meta-llama/Llama-3.3-70B-Instruct" # hugging face
-#model_id = "llama3.3" # ollama: llama3.3
-# model 3.2 3B --
+# hugging face:
+model_id = "meta-llama/Llama-3.3-70B-Instruct" # hugging face
 #model_id = "meta-llama/Llama-3.2-3B-Instruct" # hugging face
-#model_id = "llama3.2" # ollama: llama3.2
-# model 3.2 1B --
 #model_id = "meta-llama/Llama-3.2-1B-Instruct" # hugging face
-#model_id = "llama3.2:1b" # ollama: llama3.2:1b
-# model 3.1 8B --
 #model_id = "meta-llama/Llama-3.1-8B" # hugging face
-model_id = "llama3.3"
-ollama.pull(model_id) # initialize the model
+
+login(token = secrets.LLAMA_33_API_KEY)
+
+task = "text-generation"
+weights = {"torch_dtype": torch.bfloat16} # quantization = 16, 32, 64
+tokens = 30 # n characters in output
+pipeline = transformers.pipeline(task, model = model_id, model_kwargs = weights) # initate pipeline
+
 checkpoint1 = datetime.now() # start runtime counter
 
 #TOKENS = 30 # n characters in output
@@ -36,8 +38,7 @@ checkpoint1 = datetime.now() # start runtime counter
 # set input parameters
 # 1. determine which outcomes to run the model for
 ALL_OUTCOMES = ["sa", "ob", "pr", "re", "cu", "di", "w", "co"]
-OUTCOMES = ["sa", "co", "pr", "w"]
-PROMPT_FILE = start.DATA_DIR + "prompts_sa.xlsx"
+OUTCOMES = ["sa"]
 PROMPT_SHEET = "Zero Shot 3"
 
 prompt_dict = {}
@@ -49,9 +50,9 @@ for outcome in ALL_OUTCOMES:
     prompt_dict[outcome] = prompt
 
 # 2. determine how many survey responses to include
-SAMPLE_SIZE = 90
-df = pd.read_excel("home/bah17005/gold_standard_recalls.xlsx") # for the hpc interactive job
-#df = pd.read_excel(start.DATA_DIR + "clean/gold_standard_recalls.xlsx")
+SAMPLE_SIZE = 30
+#df = pd.read_excel("clean/gold_standard_recalls.xlsx") # for the hpc interactive job
+df = pd.read_excel(start.DATA_DIR + "gold_standard_recalls.xlsx")
 empty_text = []
 for row in list(range(0, len(df["recall_texts"]))):
     if (type(df["recall_texts"][row])) == float:
@@ -68,8 +69,8 @@ for outcome in OUTCOMES:
     for case in df["recall_texts"]:
         prompt_case = prompt_dict[outcome] + " " + case
         input = [{"role": "user", "content": prompt_case}]
-        output: ChatResponse = chat(model = model_id, messages = input)
-        response = output["message"]["content"]
+        output = pipeline(input, max_new_tokens = tokens)
+        response = output[0]["generated_text"][-1]["content"]
         if response.isdigit() == False:
             numbers = []
             for character in response:
@@ -78,25 +79,28 @@ for outcome in OUTCOMES:
                     number = numbers.append(number)
             response = numbers[0]
         llama_output.append(response)
+        print(response)
     df["llama_" + outcome] = llama_output
     rmse.append(fit.get_rmse(df[outcome], df["llama_" + outcome]))
 
-checkpoint3 = datetime.now() # start runtime counter
+checkpoint3 = datetime.now() # end runtime counter
 
 # get runtime and cpu, ram usage:
-checkpoint4 = datetime.now() # end runtime counter
-total_runtime = checkpoint4 - checkpoint0 # add other markers... total runtime, model request runtime
-pull_model_runtime = checkpoint2 - checkpoint1
-request_runtime = checkpoint3 - checkpoint2
+total_runtime = (checkpoint3 - checkpoint0).total_seconds() # add other markers... total runtime, model request runtime
+pull_model_runtime = (checkpoint2 - checkpoint1).total_seconds()
+request_runtime = (checkpoint3 - checkpoint2).total_seconds()
 cpu_usage = psutil.cpu_percent(total_runtime)
 ram_used = psutil.virtual_memory()[3]/1000000000 # in GB
 
 output = {"model": [model_id], "outcomes": [OUTCOMES], "cases per outcome": [SAMPLE_SIZE], "rmse": [rmse], "total runtime (min)": [total_runtime/60], "model_pull": [pull_model_runtime/60], "request": [request_runtime/60], "cpu ()": [cpu_usage], "ram (gb)": [ram_used]}
 output_csv = pd.DataFrame(output)
 
+print(df)
+print(output)
+
+# write .xlsx
+path = start.OUTPUT_DIR
 filename = model_id + "_out" + str(len(OUTCOMES)) + "_n" + str(SAMPLE_SIZE) + ".csv"
 
-# sheet 1 = llama output for gold standard cases
-df.to_csv("df_"+ filename, index = False)
-output_csv.to_csv(filename, index = False)
-# sheet 2 = rmse + metadata
+df.to_csv(path + "df_"+ filename, index = False)
+output_csv.to_csv(path + filename, index = False)
