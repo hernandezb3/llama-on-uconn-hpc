7dbbd1eb2ce4c5131772e1f26685f855a2be66c0
hernandezb3 <brittney.hernandez@uconn.edu>
2025-04-09 10:48:05 -0400

    reorg and add to bash commands

--- hpc/bash.sh
+++ hpc/bash.sh
@@ -1,21 +1,37 @@
-# after job started, use the following to check
-hostname # check node
-date # gets date and timestamp
-pwd # print working directory
-ls # list contents of working directory
-ls -a # list all contents of working directory (includes hidden files)
+# misc bash commands for navigating UConn Storrs HPC
+hostname # show the system's host name i.e., check the node
+
+pwd # pwd = print working directory
+# cd = change directory
+cd .. # change to parent
+cd ../.. # change to parent of parent
+cd ollama # change to ollama folder
+cd "home/$USER" # change to node of user
+cd "/scratch/" # change to scratch folder (where files are saved in UConn Storrs HPC)
+# by default files are stored in home/$
+
+# ls = list
+ls # list files in the working directory
+ls -a # list all files, including hidden ones
 ls -lh # list files and their sizes
 lsmod # view loaded modules
+# mkdir = make directory
+mkdir test # create a new folder called test
+# rmdir = remove directory
+rmdir test # remove the folder called test
+# rm = remove file
+rm goodreads_20.csv # remove the file called goodreads_20.csv
+# mv = move: move files and directories from one directory to another
+mv output shared # move output.csv from scratch to shared
+cat # print the contents of a file
+
+# df = disk space usage
+df -k # check the storage of the partition
 
 # ctrl + c to kill a running command
 
-# by default files are stored in home/$USER
-cd ../.. # go to parent of parent
-cd "home/$USER" # go to node of user
-cd "/scratch/"
-cd ollama
-df -k # check the storage of the partition
-mv # move files e.g., scratch > shared
-cat # print the contents of a file
+date # gets date and timestamp
+
+
 
 #git clone https://github.com/hernandezb3/crisp_hpc
