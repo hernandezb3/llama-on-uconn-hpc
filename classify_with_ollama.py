b60c07b93bf7290aa5208da68cdae18c84a6d26c
hernandezb3 <brittney.hernandez@uconn.edu>
2025-04-17 11:58:56 -0400

    correct script errors

--- classify_with_ollama.py
+++ classify_with_ollama.py
@@ -37,7 +37,6 @@ for case in df["review_text"]:
     prompt_case = prompts + " " + case
     input = [{"role": "user", "content": prompt_case}]
     response = llm.invoke(input)
-    print(response)
     if response.isdigit() is False:
         numbers = []
         for character in response:
@@ -46,27 +45,25 @@ for case in df["review_text"]:
                 number = numbers.append(number)
         response = numbers[0]
     llama_output.append(response)
+
 df["llama_rating"] = llama_output
 rmse.append(fit.get_rmse(df["rating"], df["llama_rating"]))
 
 checkpoint2 = datetime.now() # end runtime counter
-print(df)
 
 # get runtime and cpu, ram usage:
-total_runtime = checkpoint2 - checkpoint0 
-pull_model_runtime = "na"
+total_runtime = checkpoint2 - checkpoint0
 request_runtime = checkpoint2 - checkpoint1
 cpu_usage = psutil.cpu_percent(total_runtime.total_seconds())
 ram_used = psutil.virtual_memory()[3]/1000000000 # in GB
 # compile into dataframe
-output = {"model": [model_id], "n cases": [df.index], "rmse": [rmse], "total runtime (min)": [total_runtime/60], "model_pull": [pull_model_runtime/60], "request": [request_runtime/60], "cpu ()": [cpu_usage], "ram (gb)": [ram_used]}
+output = {"model": [model_id], "n cases": [df.shape[0]], "rmse": [rmse], "total runtime (min)": [total_runtime/60], "request": [request_runtime/60], "cpu ()": [cpu_usage], "ram (gb)": [ram_used]}
 output = pd.DataFrame(output)
-print(output)
 
 # write df and output to an .xlsx file
 # create an output file in the parent directory
 path = start.OUTPUT_DIR
-filename = df.index + dictionary.model_names[model_id] + ".csv"
+filename = "n" + str(df.shape[0]) + "_" + dictionary.model_names[model_id] + ".csv"
 
 df.to_csv(path + "df_demo_" + filename, index = False)
 output.to_csv(path + "output_demo_" + filename, index = False)
