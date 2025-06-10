52c39ee331f38639bcfa4cd9b136c995510172e8
hernandezb3 <brittney.hernandez@uconn.edu>
2025-06-10 09:18:26 -0400

    adding gemma 3

--- library/dictionary.py
+++ library/dictionary.py
@@ -6,7 +6,8 @@ model_names = {"meta-llama/Llama-3.3-70B-Instruct": "llama3.3",
                "llama3.3": "llama3.3",
                "llama3.2": "llama3.2",
                "llama3.2:1b": "llama3.2_1b",
-               "llama3.1": "llama3.1"}
+               "llama3.1": "llama3.1",
+               "gemma3:1b": "gemma3:1b"}
 
 demo_prompts = {"hello": "Hello, how are you?",
                 "goodreads": "Predict the rating of the following book review on a scale of 0 = bad to 5 = good. Only respond with a number 0-5."}
